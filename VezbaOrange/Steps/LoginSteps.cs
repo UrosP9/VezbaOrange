dd2462c0a057ee8af6d8c84bf1f21e327e5be329
Uros Petrovic <u.petrovic@levi9.com>
2021-10-11 17:00:50 +0200

    www

--- VezbaOrange/Steps/LoginSteps.cs
+++ VezbaOrange/Steps/LoginSteps.cs
@@ -91,23 +91,37 @@ namespace VezbaOrange
 
             LoginPage login = new LoginPage(Driver);
 
-            var user = login.UsrnmField();
-            var passw = login.PasswordField();
+            //var user = login.UsrnmField();
+            //var passw = login.PasswordField();
 
-            if (user.Text != Username && passw.Text != Password)
+            if (Username != "" && Password != "")
             {
                 Assert.IsTrue(actualvalue.Contains("Invalid credentials"));
             }
 
-            else if ((user.Text == Username && passw.Text != Password) || (user.Text == Username && passw.Text == Password))
+            else if ((Username == "" && Password != "") || (Username == "" && Password == ""))
             {
                 Assert.IsTrue(actualvalue.Contains("Username cannot be empty"));
             }
 
-            else if (user.Text != Username && passw.Text == Password)
+            else if (Username != "" && Password == "")
             {
                 Assert.IsTrue(actualvalue.Contains("Password cannot be empty"));
             }
+            //if (user.Text != Username && passw.Text != Password)
+            //{
+            //    Assert.IsTrue(actualvalue.Contains("Invalid credentials"));
+            //}
+
+                //else if ((user.Text == Username && passw.Text != Password) || (user.Text == Username && passw.Text == Password))
+                //{
+                //    Assert.IsTrue(actualvalue.Contains("Username cannot be empty"));
+                //}
+
+                //else if (user.Text != Username && passw.Text == Password)
+                //{
+                //    Assert.IsTrue(actualvalue.Contains("Password cannot be empty"));
+                //}
         }
 
 
