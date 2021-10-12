9aa37d1e1215c82405aac0892f0b6358ad7882c9
Uros Petrovic <u.petrovic@levi9.com>
2021-10-12 09:23:38 +0200

    Changed 'if'

--- VezbaOrange/Steps/LoginSteps.cs
+++ VezbaOrange/Steps/LoginSteps.cs
@@ -85,14 +85,9 @@ namespace VezbaOrange
 
 
         [Then(@"user can see errormessage for (.*) and (.*)")]
-            public void ThenUserCanSeeErrormessageForAnd(string Username, string Password)
-            {
+        public void ThenUserCanSeeErrormessageForAnd(string Username, string Password)
+        {
             string actualvalue = Driver.FindElement(By.Id("spanMessage")).Text;
-
-            LoginPage login = new LoginPage(Driver);
-
-            //var user = login.UsrnmField();
-            //var passw = login.PasswordField();
 
             if (Username != "" && Password != "")
             {
@@ -108,27 +103,8 @@ namespace VezbaOrange
             {
                 Assert.IsTrue(actualvalue.Contains("Password cannot be empty"));
             }
-            //if (user.Text != Username && passw.Text != Password)
-            //{
-            //    Assert.IsTrue(actualvalue.Contains("Invalid credentials"));
-            //}
 
-                //else if ((user.Text == Username && passw.Text != Password) || (user.Text == Username && passw.Text == Password))
-                //{
-                //    Assert.IsTrue(actualvalue.Contains("Username cannot be empty"));
-                //}
-
-                //else if (user.Text != Username && passw.Text == Password)
-                //{
-                //    Assert.IsTrue(actualvalue.Contains("Password cannot be empty"));
-                //}
         }
-
-
-
-
-        
-
 
     }
 }
